f1a31d85706866ab689f0871148f360602537959
Tiago Rodrigues <tr2111931@gmail.com>
2024-12-30 15:55:45 -0300

    PLAYMUSIC

--- src/App.java
+++ src/App.java
@@ -7,27 +7,56 @@ import dominio.Album;
 import dominio.Artista;
 import dominio.Musica;
 
+class AudioPlayer {
+    boolean isPlaying = false;
+    javax.sound.sampled.Clip audioClip;
+
+    public void loadAudio(String filePath) {
+        try {
+            java.io.File audioFile = new java.io.File(filePath);
+            javax.sound.sampled.AudioInputStream audioStream = javax.sound.sampled.AudioSystem.getAudioInputStream(audioFile);
+            audioClip = javax.sound.sampled.AudioSystem.getClip();
+            audioClip.open(audioStream);
+        } catch (Exception e) {
+            System.out.println("Erro ao carregar o áudio: " + e.getMessage());
+        }
+    }
+
+    public void playAudio() {
+        if (audioClip != null) {
+            audioClip.start();
+            isPlaying = true;
+        }
+    }
+
+    public void stopAudio() {
+        if (audioClip != null) {
+            audioClip.stop();
+            isPlaying = false;
+        }
+    }
+}
+
 public class App {
     private static int currentTrackIndex = 0;
-    private static Musica[] musicas;
 
     public static void main(String[] args) throws Exception {
         Musica musica1 = new Musica();
         musica1.setNome("Californication");
         musica1.setDuracao(120);
-        musica1.setArquivoAudio("./assets/Red-Hot-Chili-Peppers-Californication.wav");
+        musica1.setArquivoAudio("assets/Red-Hot-Chili-Peppers-Californication.wav");
         musica1.setGenero("Rock");
 
         Musica musica2 = new Musica();
         musica2.setNome("Otherside");
         musica2.setDuracao(120);
-        musica2.setArquivoAudio("./assets/Red-Hot-Chili-Peppers-Otherside.wav");
+        musica2.setArquivoAudio("assets/Red-Hot-Chili-Peppers-Otherside.wav");
         musica2.setGenero("Rock");
 
         Musica musicaDesama = new Musica();
         musicaDesama.setNome("Como É Que Desama");
         musicaDesama.setDuracao(210);
-        musicaDesama.setArquivoAudio("./assets//home/tiago/Downloads/aula-poo-playmusic-main/assets/01-Como-é-que-desama-Ramon-e-Randinho-part-Rey-Vaqueiro.wav");
+        musicaDesama.setArquivoAudio("assets/01-Como-é-que-desama.wav");
         musicaDesama.setGenero("Sertanejo");
 
         Album album1 = new Album();
@@ -43,7 +72,7 @@ public class App {
 
         System.out.println("Abrindo o PlayMusic");
         AudioPlayer player = new AudioPlayer();
-        player.loadAudio(redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getArquivoAudio());
+        player.loadAudio(album1.getMusicas().get(currentTrackIndex).getArquivoAudio());
 
         JButton playStopButton = new JButton("Play");
         playStopButton.addActionListener(new ActionListener() {
@@ -63,10 +92,11 @@ public class App {
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                currentTrackIndex = (currentTrackIndex + 1) % redHot.getAlbuns().get(0).getMusicas().size();
-                player.loadAudio(redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getArquivoAudio());
-                player.playAudio(); // Adicionei esta linha
-                System.out.println("Próxima música: " + redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getNome());
+                currentTrackIndex = (currentTrackIndex + 1) % album1.getMusicas().size();
+                player.stopAudio();
+                player.loadAudio(album1.getMusicas().get(currentTrackIndex).getArquivoAudio());
+                player.playAudio();
+                System.out.println("Próxima música: " + album1.getMusicas().get(currentTrackIndex).getNome());
             }
         });
 
@@ -74,25 +104,25 @@ public class App {
         previousButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                currentTrackIndex = (currentTrackIndex - 1 + redHot.getAlbuns().get(0).getMusicas().size()) % redHot.getAlbuns().get(0).getMusicas().size();
-                player.loadAudio(redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getArquivoAudio());
-                player.playAudio(); // Adicionei esta linha
-                System.out.println("Música anterior: " + redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getNome());
+                currentTrackIndex = (currentTrackIndex - 1 + album1.getMusicas().size()) % album1.getMusicas().size();
+                player.stopAudio();
+                player.loadAudio(album1.getMusicas().get(currentTrackIndex).getArquivoAudio());
+                player.playAudio();
+                System.out.println("Música anterior: " + album1.getMusicas().get(currentTrackIndex).getNome());
             }
         });
 
-        ImageIcon icon = new ImageIcon("./assets/music.png");
-        
+        ImageIcon icon = new ImageIcon("assets/music.png");
         JOptionPane.showOptionDialog(
                 null,
-                redHot.getAlbuns().get(0).getMusicas().get(currentTrackIndex).getNome(),
+                "Tocando agora: " + album1.getMusicas().get(currentTrackIndex).getNome(),
                 "PlayMusic",
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.PLAIN_MESSAGE,
                 icon,
-                new Object[] { playStopButton, previousButton, nextButton }, 
-               
- playStopButton);
+                new Object[]{playStopButton, previousButton, nextButton},
+                playStopButton
+        );
 
         if (player.audioClip != null) {
             player.audioClip.close();
